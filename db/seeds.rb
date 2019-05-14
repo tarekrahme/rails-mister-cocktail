927d7f72b53f2a6dca9abc5eaff564f29f14dc15
Tarek Rahme <rahmetarek@gmail.com>
2019-05-14 15:08:58 +0100

    Cocktails app part 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create(name: 'chickpeas')
+Ingredient.create(name: 'tahini')
+Ingredient.create(name: 'lemon juice')
